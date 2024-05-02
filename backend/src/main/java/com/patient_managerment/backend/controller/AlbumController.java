84b880b50f82c80345ad72c0e22bca63330e10f8
Hoang Manh Dung <manhdung18102003@gmail.com>
2024-05-02 08:44:47 +0700

    API trash, API search phone address, API update note

--- backend/src/main/java/com/patient_managerment/backend/controller/AlbumController.java
+++ backend/src/main/java/com/patient_managerment/backend/controller/AlbumController.java
@@ -1,16 +1,14 @@
 package com.patient_managerment.backend.controller;
 
 import com.patient_managerment.backend.dto.AlbumDTO;
+import com.patient_managerment.backend.dto.ImageDTO;
 import com.patient_managerment.backend.dto.ImagesDTO;
 import com.patient_managerment.backend.imp.AlbumServiceImp;
 import com.patient_managerment.backend.payload.RespondData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,13 +33,41 @@ public class AlbumController {
         return new ResponseEntity<>(respondData, HttpStatus.OK);
     }
 
+    @GetMapping("/search")
+    public ResponseEntity<?> searchInforPhoneAddress(@RequestParam String phoneAddress){
+        RespondData respondData = new RespondData();
+        respondData.setData(albumServiceImp.searchInforPhoneAddress(phoneAddress));
+        respondData.setDesc("Get infor successfully!");
+        return new ResponseEntity<>(respondData, HttpStatus.OK);
+    }
+
     @GetMapping("/get_mark")
     public ResponseEntity<?> getMarkRecords( @RequestParam boolean status){
         RespondData respondData = new RespondData();
 
         List<AlbumDTO> albumDTOList = albumServiceImp.getMarkRecord( status);
         respondData.setData(albumDTOList);
-        respondData.setDesc("Update image favourite status successfully!");
+        respondData.setDesc("Get marked images successfully!");
+        return new ResponseEntity<>(respondData, HttpStatus.OK);
+    }
+
+    @GetMapping("/get_trash")
+    public ResponseEntity<?> getTrashRecords(){
+        RespondData respondData = new RespondData();
+
+        List<AlbumDTO> albumDTOList = albumServiceImp.getTrashRecords();
+        respondData.setData(albumDTOList);
+        respondData.setDesc("Get trash successfully!");
+        return new ResponseEntity<>(respondData, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete_trash")
+    public ResponseEntity<?> deleteTrash(){
+        RespondData respondData = new RespondData();
+
+        List<AlbumDTO> albumDTOList = albumServiceImp.deleteTrash();
+        respondData.setData(albumDTOList);
+        respondData.setDesc("Delete trash successfully!");
         return new ResponseEntity<>(respondData, HttpStatus.OK);
     }
 
